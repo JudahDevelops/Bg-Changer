889e7da56b9e8f26f36ba267a8700fc23669642a
JudahDevelops <judahdevelops@gmail.com>
2024-08-06 18:57:49 +0100

    index.html edited, body given the id of background

---
+++ script.js
@@ -0,0 +1,11 @@
+const colorContainer = document.getElementById("color-container");
+const colorButton = document.getElementById("color-button");
+
+const colors = ["red", "blue", "yellow", "green", "orange", "brown", "white", "indigo", "violet"];
+
+colorButton.addEventListener("click", () => {
+    const background = document.getElementById("background");
+    background.style.backgroundColor = colors[Math.floor(Math.random() * colors.length - 1)]
+});
+
+
